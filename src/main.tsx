0756bc94c0b9c1f8214ae9df4cacc92c78d6965e
kennethryandy <dykennethryan@gmail.com>
2024-12-07 19:26:42 +0800

    update version

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import ThemeProvider from '@/components/providers/theme-provider';
+import ThemeProvider from "@/components/providers/theme-provider";
 
-import App from './app';
-import Layout from './layout';
-import './globals.css';
+import App from "./App";
+import Layout from "./layout";
+import "./globals.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <Layout>
-        <App />
-      </Layout>
-    </ThemeProvider>
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+			<Layout>
+				<App />
+			</Layout>
+		</ThemeProvider>
+	</React.StrictMode>,
 );
